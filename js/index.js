8358a726786810df0e7ce73407da571311bef755
Matthew Wang <matt@matthewwang.me>
2018-09-14 19:02:05 -0700

    laying out basics
    starting up jekyll and custom-compiling bulma

---
+++ js/index.js
@@ -0,0 +1,53 @@
+/*
+var cityList = {};
+cityList.opacityIn = [0,1];
+cityList.scaleIn = [0.2, 1];
+cityList.scaleOut = 3;
+cityList.durationIn = 800;
+cityList.durationOut = 600;
+cityList.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.city-list .letters-1',
+    opacity: cityList.opacityIn,
+    scale: cityList.scaleIn,
+    duration: cityList.durationIn
+  }).add({
+    targets: '.city-list .letters-1',
+    opacity: 0,
+    scale: cityList.scaleOut,
+    duration: cityList.durationOut,
+    easing: "easeInExpo",
+    delay: cityList.delay
+  }).add({
+    targets: '.city-list .letters-2',
+    opacity: cityList.opacityIn,
+    scale: cityList.scaleIn,
+    duration: cityList.durationIn
+  }).add({
+    targets: '.city-list .letters-2',
+    opacity: 0,
+    scale: cityList.scaleOut,
+    duration: cityList.durationOut,
+    easing: "easeInExpo",
+    delay: cityList.delay
+  }).add({
+    targets: '.city-list .letters-3',
+    opacity: cityList.opacityIn,
+    scale: cityList.scaleIn,
+    duration: cityList.durationIn
+  }).add({
+    targets: '.city-list .letters-3',
+    opacity: 0,
+    scale: cityList.scaleOut,
+    duration: cityList.durationOut,
+    easing: "easeInExpo",
+    delay: cityList.delay
+  }).add({
+    targets: '.city-list',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+	*/
